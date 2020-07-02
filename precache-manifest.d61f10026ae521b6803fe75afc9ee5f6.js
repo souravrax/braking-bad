c6af2f3f0eea9b2167b332b9de588ce8105230b5
Sourav Rakshit <38202655+souravrax@users.noreply.github.com>
2020-07-03 02:07:02 +0530

    Updates

---
+++ precache-manifest.d61f10026ae521b6803fe75afc9ee5f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c90e01a968dcbec98816c15993606f14",
+    "url": "/braking-bad/index.html"
+  },
+  {
+    "revision": "d7786ce43ede830a2828",
+    "url": "/braking-bad/static/css/main.92bb1c56.chunk.css"
+  },
+  {
+    "revision": "b26e94c6957ae6097eeb",
+    "url": "/braking-bad/static/js/2.8acc9afd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/braking-bad/static/js/2.8acc9afd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7786ce43ede830a2828",
+    "url": "/braking-bad/static/js/main.517f1be3.chunk.js"
+  },
+  {
+    "revision": "f819e556a75542a6d487",
+    "url": "/braking-bad/static/js/runtime-main.dddea2c8.js"
+  }
+]);
